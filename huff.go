8d10c112196f626852a2a73521ba68bc5c5d2d0c
Damian Gryski <damian@gryski.com>
2015-05-22 09:21:12 +0200

    initial import

---
+++ huff.go
@@ -0,0 +1,162 @@
+// Package huff is a simple huffman encoder/decoder
+package huff
+
+import (
+	"container/heap"
+	"errors"
+	"io"
+
+	"github.com/dgryski/go-bitstream"
+)
+
+var EOF uint32 = 0xffffffff
+
+type symbol struct {
+	s    uint32
+	Code uint32
+	Len  int
+}
+
+type Encoder struct {
+	eof uint32
+	m   []symbol
+}
+
+type node struct {
+	weight int
+	child  [2]*node
+	leaf   bool
+	sym    uint32
+}
+
+type nodes []node
+
+func (n nodes) Len() int            { return len(n) }
+func (n nodes) Swap(i, j int)       { n[i], n[j] = n[j], n[i] }
+func (n nodes) Less(i, j int) bool  { return n[i].weight < n[j].weight }
+func (n *nodes) Push(x interface{}) { *n = append(*n, x.(node)) }
+
+func (n *nodes) Pop() interface{} {
+	old := *n
+	l := len(old)
+	x := old[l-1]
+	*n = old[0 : l-1]
+	return x
+}
+
+func NewEncoder(counts []int) *Encoder {
+	var n nodes
+
+	for i, v := range counts {
+		heap.Push(&n, node{weight: v, leaf: true, sym: uint32(i)})
+	}
+
+	// one more for EOF
+	eof := uint32(len(counts))
+	heap.Push(&n, node{weight: 0, leaf: true, sym: eof})
+
+	for n.Len() > 1 {
+		n1 := heap.Pop(&n).(node)
+		n2 := heap.Pop(&n).(node)
+		heap.Push(&n, node{weight: n1.weight + n2.weight, child: [2]*node{&n1, &n2}})
+	}
+
+	m := make([]symbol, eof+1)
+
+	walk(&n[0], 0, 0, m)
+
+	return &Encoder{eof: eof, m: m}
+}
+
+func walk(n *node, code uint32, depth int, m []symbol) {
+
+	if n.leaf {
+		m[n.sym] = symbol{s: n.sym, Code: code, Len: depth}
+		return
+	}
+
+	walk(n.child[0], code<<1, depth+1, m)
+	walk(n.child[1], (code<<1)|1, depth+1, m)
+}
+
+func (e *Encoder) Writer(w io.Writer) *Writer {
+	return &Writer{e: e, BitWriter: bitstream.NewWriter(w)}
+}
+
+type Writer struct {
+	e *Encoder
+	*bitstream.BitWriter
+}
+
+var ErrUnknownSymbol = errors.New("huff: unknown symbol")
+
+func (w *Writer) WriteSymbol(s uint32) error {
+
+	if s == EOF {
+		s = w.e.eof
+	}
+
+	if s > w.e.eof {
+		return ErrUnknownSymbol
+	}
+
+	sym := w.e.m[s]
+
+	w.BitWriter.WriteBits(uint64(sym.Code), sym.Len)
+
+	return nil
+}
+
+type Decoder struct {
+	*bitstream.BitReader
+	m      []symbol
+	codes  map[uint32]uint32
+	eof    uint32
+	maxlen int
+}
+
+func (e *Encoder) Decoder(r io.Reader) *Decoder {
+	codes := make(map[uint32]uint32)
+
+	var max int
+
+	for i, sym := range e.m {
+		codes[sym.Code] = uint32(i)
+		if sym.Len > max {
+			max = sym.Len
+		}
+	}
+
+	return &Decoder{
+		BitReader: bitstream.NewReader(r),
+		m:         e.m,
+		codes:     codes,
+		eof:       e.eof,
+		maxlen:    max,
+	}
+}
+
+func (d *Decoder) ReadSymbol() (uint32, error) {
+	var c uint32
+
+	for i := 0; i < d.maxlen; i++ {
+		b, err := d.ReadBit()
+		if err != nil {
+			return 0, err
+		}
+
+		c <<= 1
+		if b {
+			c |= 1
+		}
+
+		if s, ok := d.codes[c]; ok {
+			if s == d.eof {
+				s = EOF
+			}
+			return s, nil
+		}
+	}
+
+	return 0, ErrUnknownSymbol
+}
---
+++ huff_test.go
@@ -0,0 +1,60 @@
+package huff
+
+import (
+	"bytes"
+	"testing"
+
+	"gopkg.in/bufio.v1"
+)
+
+func TestHuff(t *testing.T) {
+
+	counts := []int{3, 1, 4, 1, 5, 9}
+
+	e := NewEncoder(counts)
+
+	var buf bufio.Buffer
+
+	w := e.Writer(&buf)
+
+	stream := []uint32{5, 3, 1, 1, 3}
+
+	for i := range stream {
+		w.WriteSymbol(stream[i])
+		w.WriteBits(uint64(0xfff), int(stream[i]))
+	}
+
+	w.WriteSymbol(EOF)
+	w.Flush(false)
+
+	d := e.Decoder(bytes.NewReader(buf.Bytes()))
+
+	var i int
+	var foundEOF bool
+	for {
+		s, err := d.ReadSymbol()
+		if err != nil {
+			break
+		}
+		if s == EOF {
+			foundEOF = true
+			break
+		}
+		if i >= len(stream) {
+			break
+		}
+		if s != stream[i] {
+			t.Errorf("stream index %d = %d, want %d", i, s, stream[i])
+		}
+		b, _ := d.ReadBits(int(s))
+		want := uint64(1<<s) - 1
+		if b != want {
+			t.Errorf("stream item %d = %x, want %x", i, b, want)
+		}
+		i++
+	}
+
+	if !foundEOF {
+		t.Errorf("did not find expected EOF token")
+	}
+}
